41919a296dca4b326b418585fa6406890796c3e4
antjuntu <antjuntu@gmail.com>
2019-03-07 19:16:03 +0200

    2.6: puhelinluettelo step1

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -6,18 +6,41 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const personRows = () => {
+    return persons.map(person =>
+      <div key={person.name}>{person.name}</div>
+    )
+  }
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          nimi: <input />
+          nimi: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
+      <div>
+        {personRows()}
+      </div>
     </div>
   )
 
