983be14d3b157d6e6d943161aa57b9dcc4b62058
antjuntu <antjuntu@gmail.com>
2019-03-09 14:06:35 +0200

    maiden tiedot starting code

---
+++ maidentiedot/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          find countries <input />
+        </div>
+      </form>
+      <div>Too many countries, specify another filter</div>
+    </div>
+  )
+}
+
+export default App
